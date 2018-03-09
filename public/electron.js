e8038cc9cfa8d76d20ddd3c7389634010e06698f
danielm-ridewta <DanielM@whatcomtrans.net>
2018-03-09 11:13:56 -0800

    Add production executable build.

--- public/electron.js
+++ public/electron.js
@@ -23,7 +23,15 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadURL("http://localhost:3000");
+  const startUrl =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : url.format({
+          pathname: path.join(__dirname, "/../build/index.html"),
+          protocol: "file:",
+          slashes: true,
+        });
+  mainWindow.loadURL(startUrl);
   /*
   mainWindow.loadURL(
     url.format({
