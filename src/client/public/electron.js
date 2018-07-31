dd299b2f6b51749d0b05a57dab1cd97b8eb82a29
danielm-ridewta <danielm@whatcomtrans.net>
2018-07-31 11:45:27 -0700

    Add auto-update

--- src/client/public/electron.js
+++ src/client/public/electron.js
@@ -2,6 +2,7 @@ const electron = require("electron");
 const path = require("path");
 const url = require("url");
 const logger = require("electron-log");
+const { autoUpdater } = require("electron-updater");
 const { app, BrowserWindow, Tray, Menu } = electron;
 
 logger.transports.file.level = "info";
@@ -50,11 +51,82 @@ function createWindow() {
   });
 }
 
+let win;
+
+function sendStatusToWindow(text) {
+  log.info(text);
+  win.webContents.send("message", text);
+}
+
+autoUpdater.on("checking-for-update", () => {
+  sendStatusToWindow("Checking for update...");
+});
+
+autoUpdater.on("update-available", info => {
+  sendStatusToWindow("Update available.");
+});
+
+autoUpdater.on("update-not-available", info => {
+  sendStatusToWindow("Update not available.");
+});
+
+autoUpdater.on("error", err => {
+  sendStatusToWindow("Error in auto-updater. " + err);
+});
+
+autoUpdater.on("download-progress", progressObj => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + " - Downloaded " + progressObj.percent + "%";
+  log_message =
+    log_message +
+    " (" +
+    progressObj.transferred +
+    "/" +
+    progressObj.total +
+    ")";
+  sendStatusToWindow(log_message);
+});
+
+autoUpdater.on("update-downloaded", info => {
+  sendStatusToWindow("Update downloaded");
+});
+
+app.on("ready", function() {
+  logger.info("Application ready");
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  autoUpdater.checkForUpdatesAndNotify();
+  logger.info("Creating Window");
+
+  createWindow();
+});
+
+/*
 app.on("ready", () => {
   logger.info("Application ready");
   logger.info("Creating Window");
+  autoUpdater.checkForUpdates();
   createWindow();
 });
+*/
+// app.on('ready', function()  {
+//   autoUpdater.checkForUpdates();
+// });
+// autoUpdater.on('checking-for-update', () => {
+// })
+// autoUpdater.on('update-available', (info) => {
+// })
+// autoUpdater.on('update-not-available', (info) => {
+// })
+// autoUpdater.on('error', (err) => {
+// })
+// autoUpdater.on('download-progress', (progressObj) => {
+// })
+// autoUpdater.on('update-downloaded', (info) => {
+//   autoUpdater.quitAndInstall();
+// })
 
 app.on("window-all-closed", () => {
   app.quit();
